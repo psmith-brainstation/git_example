b9636df7827edf77bf817611e07ef20ac26d0825
Paul Smith <psmith@MBPl21LCQF0G26XL.local>
2022-10-31 12:43:03 -0400

    added basic program and config files

---
+++ src/my_program.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def my_function():
+    logger.info("Hello World")
+
+
+if __name__ == "__main__":
+    my_function()
