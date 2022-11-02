b8d932edc345201b6d883a809d6f58b219b37b1f
Joshua Cohn <joshcohn3@gmail.com>
2022-11-02 15:06:12 -0400

    A new module named new_module.py, Input for function requires your Name

---
+++ src/new_module.py
@@ -0,0 +1,14 @@
+def new_function(param):
+    assert isinstance(param,str), "Input must be a string"
+    name_list=param.split(" ")
+    new_name=[]
+    for string in name_list:
+        if len(string)>1:
+            dummy=list(string)
+            dummy2=string[0]
+            dummy[0]=dummy[-1]
+            dummy[-1]=dummy2
+            new_name.append("".join(dummy)+"ay")
+    final_name=" ".join(new_name)
+    print(f'Your Pig Latin Name is {final_name}')
+    return final_name
